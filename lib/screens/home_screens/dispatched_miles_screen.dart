23e138a1b1197eb8051fb6bd8b41593ef996b2d1
Gul-Is-Here <gulfarazahmed58@gmail.com>
2024-06-01 02:18:57 +0500

    'This Section Completed'

--- lib/screens/home_screens/dispatched_miles_screen.dart
+++ lib/screens/home_screens/dispatched_miles_screen.dart
@@ -2,134 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:dispatched_calculator_app/controllers/home_controller.dart';
 import 'package:velocity_x/velocity_x.dart';
-
 import '../../widgets/custome_textFormField.dart';
 
-class LoadScreen extends StatefulWidget {
+class LoadScreen extends StatelessWidget {
   final HomeController homeController;
 
   LoadScreen({required this.homeController});
 
   @override
-  _LoadScreenState createState() => _LoadScreenState();
-}
-
-class _LoadScreenState extends State<LoadScreen> {
-  List<Map<String, TextEditingController>> loadControllers = [];
-
-  @override
-  void initState() {
-    super.initState();
-    addLoad();
-  }
-
-  void addLoad() {
-    loadControllers.add({
-      'freightChargeController': TextEditingController(),
-      'dispatchedMilesController': TextEditingController(),
-      'estimatedTollsController': TextEditingController(),
-      'otherCostsController': TextEditingController(),
-    });
-  }
-
-  @override
-  void dispose() {
-    for (var controllers in loadControllers) {
-      controllers.values.forEach((controller) => controller.dispose());
-    }
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print(loadControllers.length);
-    print(widget.homeController.freightChargeController.value);
     return Scaffold(
       appBar: AppBar(
         title: Text('Additional Costs'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: loadControllers.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Load ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      buildTextFormField(
-                        controller: loadControllers[index]
-                            ['freightChargeController']!,
-                        label: 'Freight Charge (\$)',
-                        hint: 'e.g., \$1000',
-                        validator: widget.homeController.validateInput,
-                      ),
-                      buildTextFormField(
-                        controller: loadControllers[index]
-                            ['dispatchedMilesController']!,
-                        label: 'Dispatched Miles',
-                        hint: 'e.g., 2000',
-                        validator: widget.homeController.validateInput,
-                      ),
-                      buildTextFormField(
-                        controller: loadControllers[index]
-                            ['estimatedTollsController']!,
-                        label: 'Estimated Tolls (\$)',
-                        hint: 'e.g., \$50',
-                        validator: widget.homeController.validateInput,
-                      ),
-                      buildTextFormField(
-                        controller: loadControllers[index]
-                            ['otherCostsController']!,
-                        label: 'Other Costs (\$)',
-                        hint: 'e.g., \$100',
-                        validator: widget.homeController.validateInput,
-                      ),
-                      20.heightBox,
-                    ],
-                  );
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                bool? addAnotherLoad = await showDialog<bool>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Add Another Load?'),
-                      content: Text('Do you want to add another load?'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(false);
-                          },
-                          child: Text('No'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(true);
-                          },
-                          child: Text('Yes'),
-                        ),
-                      ],
-                    );
-                  },
-                );
+        child: Obx(
+          () => ListView(
+            children: [
+              for (int i = 0;
+                  i < homeController.freightChargeControllers.length;
+                  i++) ...[
+                Text('Load ${i + 1}',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                10.heightBox,
+                buildTextFormField(
+                  controller: homeController.freightChargeControllers[i],
+                  label: 'Freight Charge (\$)',
+                  hint: 'e.g., \$1000',
+                  validator: homeController.validateInput,
+                ),
+                buildTextFormField(
+                  controller: homeController.dispatchedMilesControllers[i],
+                  label: 'Dispatched Miles',
+                  hint: 'e.g., 2000',
+                  validator: homeController.validateInput,
+                ),
+                buildTextFormField(
+                  controller: homeController.estimatedTollsControllers[i],
+                  label: 'Estimated Tolls (\$)',
+                  hint: 'e.g., \$50',
+                  validator: homeController.validateInput,
+                ),
+                buildTextFormField(
+                  controller: homeController.otherCostsControllers[i],
+                  label: 'Other Costs (\$)',
+                  hint: 'e.g., \$100',
+                  validator: homeController.validateInput,
+                ),
+                20.heightBox,
+              ],
+              ElevatedButton(
+                onPressed: () async {
+                  homeController.calculateVariableCosts();
 
-                if (addAnotherLoad == true) {
-                  setState(() {
-                    addLoad();
-                  });
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ],
+                  final snackBar = SnackBar(
+                    content: Text('Do you want to add another load?'),
+                    action: SnackBarAction(
+                      label: 'Add',
+                      onPressed: () {
+                        homeController.addNewLoad();
+                      },
+                    ),
+                  );
+
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                child: Text('Calculate Total Cost'),
+              ),
+              20.heightBox,
+              Obx(() => Column(
+                    children: [
+                      Text(
+                          'Total Cost Per Week: \$${homeController.totalMileageAndWeeklyFixedCost.value.toStringAsFixed(2)}'),
+                      Text(
+                          'Profit: \$${homeController.totalMileageAndWeeklyFixedCost.value.toStringAsFixed(2)}'),
+                    ],
+                  )),
+            ],
+          ),
         ),
       ),
     );
